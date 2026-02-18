ba02f84ce2442522436a38df4012649422074071
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-02-18 14:37:49 +0000

    class work

---
+++ Unit 1/pythonOop.py
@@ -0,0 +1,33 @@
+# OOP = Object Oriented Programming.
+
+# An object is a blueprint for describing something.
+
+# Computer Object Descriptors
+# It can tell time
+# It can access the internet / search up things
+# There's a power button
+# It has programs
+# You can play games on it
+# It has a screen that shows information
+# Its usually rectangular
+# You interact with it using a mouse and keyboard
+
+
+class ComputerItem:
+    def_init_(self, color, name, shape, soundOutput, portabillity, brand, ram, storage, processor, camera):
+        self.name = name
+        self.color = color
+        self.shape = shape
+        self.storage = storage
+        self.portabillity
+        self.camera = camera
+        self.soundOutput = soundOutput
+        self.brand = brand
+        self.ram = ram
+        self.processor = processor
+
+        def tatoos(tatooArt):
+            print()
+        
+apple_1 = Computers("apple m4", "black", 10.00, 320, True, True, 120, 'm4')
+apple_2 = Computers("apple m4", "white", 10.00, 320, True, True, 80, 'm4')
