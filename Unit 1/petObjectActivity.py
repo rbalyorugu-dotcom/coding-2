12810f35606ceab1776f5e2eb7bf746349f5a100
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-02-20 14:39:20 +0000

    class work

---
+++ Unit 1/petObjectActivity.py
@@ -0,0 +1,27 @@
+class PetAnimal:
+    def_init_(self, color, age, species, weight, name)
+        self.color = color
+        self.age = age
+        self.species = species
+        self.weight
+        self.name
+
+def feedPet(self):
+    print(self.name + 'is hungry')
+    print("time to feed your pet.")
+
+def sleeping(self):
+    print(self.name + 'is tired')
+    print("our pet is resting")
+
+def playtime(self):
+    print(self.name + 'is bored')
+    print("Our pet wants to play a game.")
+
+def bathroom(self):
+    print(self.name + 'needs to deficate')
+    print("needs to relieve itself")
+
+p1 = PetAnimal(blue, 23, "Macaw", 3.5lbs, "Sam")
+p2 = PetAnimal(orange, 25, "Tiger", 680lbs, "Jack")
+p3 = PetAnimal(brown, 4, "Dog", 55lbs, "Brutus")
