ab359b6899ca2a802dcf1969154fac4da351ba45
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-02-25 14:27:01 +0000

    class work

---
+++ Unit 1/bankSystem.py
@@ -0,0 +1,29 @@
+class IanFirstBank:
+    def_init_(name, self, password, address, balance):
+        self.name = name
+        self.password = password
+        self.address = address
+        self.balance = balance
+
+    def deposit(self, balance, amount):
+        print('')
+
+    def withdraw(self, balance, amount):
+        print('')
+
+    def encryptionMiddleware(self, name, password, password):
+        password = 123ABC
+        encrypt = 
+
+    def closeBankAccount(self):
+
+
+    def accessAccount(self, name, password):
+        print('')
+        # send all user info
+
+    def transfer(self):
+        print('')
+
+account_1 = RobertFirstBank('John', '123ABC', 'NA', 100)
+account_2 = RobertFirstBank('Ashley', '321ABC', 'NA', 200)
