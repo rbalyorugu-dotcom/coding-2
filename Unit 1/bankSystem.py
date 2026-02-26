2ffe0d2ea98a7ab7edad0d1443818b5de1878717
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-02-26 14:37:12 +0000

    class work

--- Unit 1/bankSystem.py
+++ Unit 1/bankSystem.py
@@ -5,11 +5,13 @@ class IanFirstBank:
         self.address = address
         self.balance = balance
 
-    def deposit(self, balance, amount):
-        print('')
+    def deposit(self, amount):
+        self.balance += amount
+        print('thanks '+ self.name +'your new balance is ' + str(self.balance))
 
     def withdraw(self, balance, amount):
-        print('')
+        self.balance -= amount
+        print('thanks '+ self.name + 'your new balance is ' + str(self.balance))
 
     def encryptionMiddleware(self, name, password, password):
         password = 123ABC
@@ -25,5 +27,23 @@ class IanFirstBank:
     def transfer(self):
         print('')
 
-account_1 = RobertFirstBank('John', '123ABC', 'NA', 100)
-account_2 = RobertFirstBank('Ashley', '321ABC', 'NA', 200)
+account_1 = IanFirstBank('John', '123ABC', 'NA', 100)
+account_2 = IanFirstBank('Ashley', '321ABC', 'NA', 200)
+
+account_1.deposit(50)
+account_2.withdraw(60)
+
+def createAccount():
+    print("Welcome to Ian's first bank.")
+    print("please complete the following questions to create your account: ")
+    name = input("name: ")
+    password = input("password: ")
+    address = input("address: ")
+    balance = input("How much would you like to add to the new account?: ")
+
+print("Congratulations! Your bank account is done.")
+
+    account_3 = IanFirstBank(name, password, address, balance)
+    users.append(account_3)
+
+createAccount()
