eae262173facd97abbb20692147bb59869b5026c
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-03-05 14:37:37 +0000

    class work

---
+++ Unit 2/pythonLibraries.py
@@ -0,0 +1,14 @@
+# Library - stores books and information --> generally quite large
+
+# Python Library - a file folder that contains objects, functions, and data
+# that we can access quickly. These collections of logic are created by other
+# prgrammers who wanted to share their code, to make easier for others.
+
+# Modules == Libraries
+
+import random
+
+print(random.randrange(1, 10))
+
+# modules/ libraries help us to speed up development time and write less code.
+# because someone already wrote it for us.
