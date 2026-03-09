325df5f373c55ad52d44d8dcf159740ee99337ae
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-03-09 13:18:28 +0000

    class work

---
+++ Unit 2/moduleActivity.py
@@ -0,0 +1,3 @@
+#What is a phython module/library?
+"A python module/library is a collection of unique functions that are already written for us to use."
+
---
+++ Unit 2/userModule.py
@@ -0,0 +1,17 @@
+class CarModel:
+    def __init__(self, color, model, speed):
+        self.color = color
+        self.model = model
+        self.speed = speed
+
+        def drive(self, model, speed):
+            print(self.model + "is driving at" + self.speed + "50 mph"):
+
+        def color(self, color, model):
+            print(self.model + "is" + "red")
+
+        def brake(self, model):
+            if brakes are pushed:
+                print(self.model + "is stopping")
+
+class CarModel(Ferrari f80)
