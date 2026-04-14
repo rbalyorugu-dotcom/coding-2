4c4f74160103ea48aaa536330cfb1d6d20472407
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-04-14 13:33:12 +0000

    class work API

--- Unit 3/placeholder.Api_3.py
+++ Unit 3/placeholder.Api_3.py
@@ -1,7 +1,34 @@
+
+
+
+
+
+
+import requests
+
 query = "https://pokeapi.co/api/v2/pokemon/tangela"
 response = requests.get(query)
 
 print(response)
-print(response["name]")
+print(response.status_code)
 
-data = json.loads(response.json())
+data = response.json()
+
+if response.status_code == 200:
+    data = response.json()
+    # print(data)
+
+    filtered_data = {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": data["types"],
+        "abilities": [ability["ability"]["name"] for ability in data["abilities"]],
+        "sprites": data["sprites"]
+    }
+
+    print(filtered_data)
+else:
+    print("data not found")
+    print("response.status_code")
+
