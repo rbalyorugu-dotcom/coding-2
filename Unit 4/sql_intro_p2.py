c95cce6e0fa426bb6244c6e969947b33230614f6
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-04-29 13:37:54 +0000

    sql database class work

--- Unit 4/sql_intro_p2.py
+++ Unit 4/sql_intro_p2.py
@@ -2,25 +2,32 @@
 import sqlite3
 
 # 2. the connect method starts our database
-connection = sqlite3.connect()
+connection = sqlite3.connect("testDb_p2.db")
 
 # 3. this cursor variable creates a new object that let's us send objects to our database
-cursor = connect.cursor()
+cursor = connection.cursor()
 
 # 4. We need to create a schema (structure) for our data
 cursor.execute('''
-    CREATE TABLE computers(
+    CREATE TABLE computer(
     id INTEGER PRIMARY KEY,
     model TEXT,
     color TEXT,
     hasWebcam BOOL,
     memory INTEGER,
-    price INTEGER,                                                               
+    price INTEGER                                                               
                )''')
 
 cursor.execute('''
     INSERT INTO computer(model, color, hasWebcam, memory, price)
     VALUES('apple m4', 'blue', True, 8, 1500)''')
 
-connect.comit()
-connect.close()
+# UPDATE A COMPUTER
+cursor.execute('''
+    UPDATE computer
+    SET price = 1300
+    WHERE model = 'Asus' ''')
+
+connection.commit()
+
+connection.close()
