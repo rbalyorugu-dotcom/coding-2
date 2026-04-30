18d7ce1f93e2bfda7ac56cc67d958cdf2674061c
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-04-30 13:37:29 +0000

    sql class work

--- Unit 4/sql_intro_p2.py
+++ Unit 4/sql_intro_p2.py
@@ -8,26 +8,50 @@ connection = sqlite3.connect("testDb_p2.db")
 cursor = connection.cursor()
 
 # 4. We need to create a schema (structure) for our data
-cursor.execute('''
-    CREATE TABLE computer(
-    id INTEGER PRIMARY KEY,
-    model TEXT,
-    color TEXT,
-    hasWebcam BOOL,
-    memory INTEGER,
-    price INTEGER                                                               
-               )''')
+# cursor.execute('''
+#     CREATE TABLE computer(
+#     id INTEGER PRIMARY KEY,
+#     model TEXT,
+#     color TEXT,
+#     hasWebcam BOOL,
+#     memory INTEGER,
+#     price INTEGER                                                               
+#                )''')
 
 cursor.execute('''
     INSERT INTO computer(model, color, hasWebcam, memory, price)
-    VALUES('apple m4', 'blue', True, 8, 1500)''')
+    VALUES('apple m4', 'blue', True, 8, 1500)
+               ''')
 
-# UPDATE A COMPUTER
+# CREATE A COMPUTER
 cursor.execute('''
-    UPDATE computer
-    SET price = 1300
-    WHERE model = 'Asus' ''')
+    INSERT INTO computer(model, color, hasWebcam, memory, price)
+    VALUES('Asus', 'black', False, 16, 1300)''')
 
 connection.commit()
 
 connection.close()
+
+# UPDATE A COMPUTER
+cursor.execute('''
+    UPDATE computer
+    SET price = 3000
+    SET color = 'orange'
+    WHERE id = 2
+               ''')
+
+# CREATE A COMPUTER
+cursor.execute('''
+    UPDATE computer
+    SET price = 1000
+    SET color = 'red'
+    WHERE id = 1
+               ''')
+    
+# CREATE A COMPUTER
+cursor.execute('''
+    UPDATE computer
+    SET price = 2030
+    SET color = 'purple'
+    WHERE id = 3
+               ''')
